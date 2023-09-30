9720b639230a63f0abb91f844d500f42b2f3afc1
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-09-30 21:05:07 +0800

    feat: add redundant object

--- objects.js
+++ objects.js
@@ -9,4 +9,25 @@ var userOne = {
     }
 }
 
-console.log(userOne)
+var userTwo = {
+    email: 'yoshi@ninjas.com',
+    name: 'Yoshi',
+    login(){
+        console.log(this.email, 'has logged in')
+    },
+    logout(){
+        console.log(this.email, 'has logged out')
+    }
+}
+
+var userThree = {
+    email: 'garo@ninjas.com',
+    name: 'Garo',
+    login(){
+        console.log(this.email, 'has logged in')
+    },
+    logout(){
+        console.log(this.email, 'has logged out')
+    }
+}
+
