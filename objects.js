314fe0dccf3a9b67bb778462c246ea34f9943c43
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-10-01 13:56:06 +0800

    feat: initiate classes, with method

--- objects.js
+++ objects.js
@@ -1,33 +1,24 @@
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
+class User {
+    constructor(email, userName){
+        this.email = email
+        this.name =  userName
+    }
     login(){
         console.log(this.email, 'has logged in')
-    },
+    }
     logout(){
         console.log(this.email, 'has logged out')
     }
 }
 
-var userTwo = {
-    email: 'yoshi@ninjas.com',
-    name: 'Yoshi',
-    login(){
-        console.log(this.email, 'has logged in')
-    },
-    logout(){
-        console.log(this.email, 'has logged out')
-    }
-}
 
-var userThree = {
-    email: 'garo@ninjas.com',
-    name: 'Garo',
-    login(){
-        console.log(this.email, 'has logged in')
-    },
-    logout(){
-        console.log(this.email, 'has logged out')
-    }
-}
+var userOne = new User('ryu@ninjas.com', 'Ryu')
+var userTwo = new User('ken@ninjas.com', 'Ken')
 
+console.log(userOne.login())
+console.log(userTwo.logout())
+
+// the 'new' keyword
+// - creates a new empty object {}
+// - sets the value of 'this' to be the new empty object
+// - calls the constrcutor method
