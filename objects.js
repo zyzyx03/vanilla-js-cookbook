d31b3fce955199d79428aa19ae5fe3908d4f213a
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-10-01 15:44:04 +0800

    feat: add method chaining

--- objects.js
+++ objects.js
@@ -2,12 +2,20 @@ class User {
     constructor(email, userName){
         this.email = email
         this.name =  userName
+        this.score = 0
     }
     login(){
         console.log(this.email, 'has logged in')
+        return this
     }
     logout(){
         console.log(this.email, 'has logged out')
+        return this
+    }
+    updateScore(){
+        this.score ++
+        console.log(this.email, 'score is now', this.score)
+        return this
     }
 }
 
@@ -15,8 +23,7 @@ class User {
 var userOne = new User('ryu@ninjas.com', 'Ryu')
 var userTwo = new User('ken@ninjas.com', 'Ken')
 
-console.log(userOne.login())
-console.log(userTwo.logout())
+userOne.login().updateScore().updateScore().logout()
 
 // the 'new' keyword
 // - creates a new empty object {}
