2a9b861ed19471fef3bb7ca0b7d5897af971d3b3
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-10-08 15:05:05 +0800

    feat: using constructor instead of class

--- objects.js
+++ objects.js
@@ -1,60 +1,15 @@
-class User {
-    constructor(email, userName){
-        this.email = email
-        this.name =  userName
-        this.score = 0
-    }
-    login(){
+function User(email, name){
+    this.email = email
+    this.name = name
+    this.online = false
+    this.login = function(){
         console.log(this.email, 'has logged in')
-        return this
-    }
-    logout(){
-        console.log(this.email, 'has logged out')
-        return this
-    }
-    updateScore(){
-        this.score ++
-        console.log(this.email, 'score is now', this.score)
-        return this
     }
 }
-
-class Admin extends User{
-    sudo(){
-        console.log("I'am Super Power")
-        return this
-    }
-    deleteUser(user){
-        users = users.filter(u => {
-            return u.email != user.email
-        })
-    }
-}
-
 
 
 var userOne = new User('ryu@ninjas.com', 'Ryu')
 var userTwo = new User('ken@ninjas.com', 'Ken')
 
-// This is method chaining
-userOne.login().updateScore().updateScore().logout()
-
-// Testing out class inheritence
-var superUper = new Admin('super@ninjas.com', 'Super')
-
-// Array of Users
-let users = [userOne, userTwo, superUper]
-
-// Testing out new extended method
-superUper.login().sudo().deleteUser(userOne)
-
-console.log(users)
-
-
-
-
-
-// the 'new' keyword
-// - creates a new empty object {}
-// - sets the value of 'this' to be the new empty object
-// - calls the constrcutor method
+console.log(userOne)
+userTwo.login()
