76b7d35f6cd591af15fdb7d4530946fc9db663b8
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-10-10 02:40:24 +0800

    feat: prototype under the hood

--- objects.js
+++ objects.js
@@ -16,10 +16,25 @@ User.prototype.logout = function(){
     console.log(this.email, 'has logged out')
 }
 
-// Attach method to User prototype
+function Admin(...args){
+    User.apply(this, args)
+    this.role = 'super admin'
+}
 
+Admin.prototype = Object.create(User.prototype)
+
+// If email match return false, if email dont match return true
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.email != u.email
+    })
+}
+
+// Attach method to User prototype
 var userOne = new User('ryu@ninjas.com', 'Ryu')
 var userTwo = new User('ken@ninjas.com', 'Ken')
+var admin = new Admin('wan@ninjas.com', 'wan')
 
-console.log(userOne)
-userTwo.login()
+var users = [userOne, userTwo, admin]
+
+console.log(admin)
