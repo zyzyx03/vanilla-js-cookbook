3c620c5ff4257ea09841f6fac05d2d950737ab44
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-10-10 01:55:48 +0800

    feat: bringing method to prototype

--- objects.js
+++ objects.js
@@ -2,11 +2,21 @@ function User(email, name){
     this.email = email
     this.name = name
     this.online = false
-    this.login = function(){
-        console.log(this.email, 'has logged in')
-    }
+    // this.login = function(){
+    //     console.log(this.email, 'has logged in')
+    // }
 }
 
+User.prototype.login = function(){
+    this.online = true
+    console.log(this.email, 'has logged in')
+}
+User.prototype.logout = function(){
+    this.online = false
+    console.log(this.email, 'has logged out')
+}
+
+// Attach method to User prototype
 
 var userOne = new User('ryu@ninjas.com', 'Ryu')
 var userTwo = new User('ken@ninjas.com', 'Ken')
